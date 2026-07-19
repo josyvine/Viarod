59bf36d7ffc708ad8d64850dd279e32336da9717
josyvine <64621103+josyvine@users.noreply.github.com>
2026-07-20 00:18:21 +0530

    chore: clean up whitespace and formatting
    Remove trailing spaces and fix minor indentation issues across the project.

--- app/src/main/java/com/viaro/activities/MeetMapActivity.java
+++ app/src/main/java/com/viaro/activities/MeetMapActivity.java
@@ -3,7 +3,7 @@ package com.viaro.activities;
 import android.content.Context;
 import android.graphics.Color;
 import android.location.Location;
-import android.os.Bundle; 
+import android.os.Bundle;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
